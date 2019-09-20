1c40a6e6fad65404acc06fc2541a8e30785ff63c
vunguyen12 <54542419+vunguyen12@users.noreply.github.com>
2019-09-20 10:31:59 -0500

    working

--- src/GameZone/RockPaperScissors.java
+++ src/GameZone/RockPaperScissors.java
@@ -1,7 +1,39 @@
 package GameZone;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class RockPaperScissors {
     public static void main(String[] args){
-        
+        String userchoice = null ;
+        int userchoicenum;
+        String comIA = null;
+        int computerIA;
+
+        Random rand = new Random();
+        Scanner input = new Scanner(System.in);
+        System.out.println("1) for scissor \n 2) for rock \n 3) for paper");
+        userchoicenum = input.nextInt();
+        computerIA = rand.nextInt(3)+1;
+
+
+//computer play
+        if (computerIA == 1)
+            comIA = "Scissor";
+        else if (computerIA == 2)
+            comIA = "Rock";
+        else if( computerIA == 3)
+            comIA = " Paper";
+//person play
+        if (userchoicenum == 1){
+            userchoice = " Scissor";
+        }
+        else if (userchoicenum == 2)
+            userchoice = "Rock";
+        else if (userchoicenum == 3)
+            userchoice = "Paper";
+// battle
+        if (userchoicenum == 1)
+        }
     }
-}
+
